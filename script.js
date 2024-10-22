148ecd13a52229ae2a2886a2ae4214635fd7a1be
Dario_W <wasik.dariusz24@gmail.com>
2024-10-22 14:20:42 +0200

    add button saving new book info and pushin it into myLibrary array as new Book object

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const myLibrary = []
 
 //the constructor function
-function Book(title, author, pages, read, info) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = info;
+    // this.info = info;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(a) {
+    myLibrary.push(a);
 
 }
 
@@ -22,6 +23,37 @@ const buttonNew = document.querySelector("#new");
 
 buttonNew.addEventListener('click', () => {
     dialogWindow.showModal();
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.checked = false;
 });
 
-// dialogWindow.showModal();
+dialogWindow.showModal();
+
+const confirmBtn = document.getElementById('confirm');
+
+let currentBook = [];
+const author = document.querySelector('#author');
+const title = document.querySelector('#title');
+const pages = document.querySelector('#pages')
+const read = document.querySelector('#read')
+
+let newBook = {}
+
+confirmBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    currentBook.push(title.value);
+    currentBook.push(author.value);
+    currentBook.push(pages.value);
+    currentBook.push(read.checked)
+
+    console.log(currentBook);    
+    newBook = new Book(currentBook[0], currentBook[1], currentBook[2], currentBook[3])
+    addBookToLibrary(newBook) 
+    console.log(newBook)
+    console.log('library', myLibrary)
+    newBook = {};
+    currentBook = [];
+    dialogWindow.close();
+} )
