629f2698eea31d4a30804235c7af80139872a4e2
Dario_W <wasik.dariusz24@gmail.com>
2024-10-22 12:00:34 +0200

    setup basic html and add button which open dialog window to add new book

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const myLibrary = []
 
+//the constructor function
+function Book(title, author, pages, read, info) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = info;
+}
+
+function addBookToLibrary() {
+
+}
+
+const dialogWindow = document.getElementById("dialogWindow");
+
+// function openDialog() {
+//     dialogWindow.showModal()
+// }
+const buttonNew = document.querySelector("#new");
+
+buttonNew.addEventListener('click', () => {
+    dialogWindow.showModal();
+});
+
+// dialogWindow.showModal();
