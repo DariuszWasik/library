dfde3305f27d722d45c1c696d2fbe466ede55faf
Dario_W <wasik.dariusz24@gmail.com>
2024-10-23 12:31:29 +0200

    add putbooksonscreen fct but have to fix repeating alredy existing box by addig new one

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = []
 
+
+
 //the constructor function
 function Book(title, author, pages, read) {
     this.title = title;
@@ -9,6 +11,12 @@ function Book(title, author, pages, read) {
     // this.info = info;
 }
 
+const book1 = new Book ('Robinson Crusoe', 'Daniel Defoe', 357, true);
+const book2 = new Book ('Lord of the Rings', 'JRR Tolkien', 1265, true);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
 function addBookToLibrary(a) {
     myLibrary.push(a);
 
@@ -16,9 +24,7 @@ function addBookToLibrary(a) {
 
 const dialogWindow = document.getElementById("dialogWindow");
 
-// function openDialog() {
-//     dialogWindow.showModal()
-// }
+
 const buttonNew = document.querySelector("#new");
 
 buttonNew.addEventListener('click', () => {
@@ -29,17 +35,18 @@ buttonNew.addEventListener('click', () => {
     read.checked = false;
 });
 
-dialogWindow.showModal();
+// dialogWindow.showModal();
 
 const confirmBtn = document.getElementById('confirm');
-
-let currentBook = [];
 const author = document.querySelector('#author');
 const title = document.querySelector('#title');
 const pages = document.querySelector('#pages')
 const read = document.querySelector('#read')
 
+let currentBook = [];
 let newBook = {}
+const singleBook = document.querySelector('.book');
+const container = document.querySelector('.books');
 
 confirmBtn.addEventListener('click', (e) =>{
     e.preventDefault();
@@ -56,4 +63,25 @@ confirmBtn.addEventListener('click', (e) =>{
     newBook = {};
     currentBook = [];
     dialogWindow.close();
+    // container.innerHTML = `<button id="new">add new book</button>`
+    // const buttonNew = document.querySelector("#new");
+    putBooksOnScreen();
 } )
+
+function putBooksOnScreen () {
+    // console.log(container.innerHTML)
+    myLibrary.forEach((book) => {
+        let div =  document.createElement('div');
+       div.className = 'book';
+       let content = `
+        <h3>${book.title}</h3>
+            <h5>by ${book.author}</h5>
+            <p>${book.pages } pages</p>
+            <p>Read? ${book.read}</p>
+            `
+            div.innerHTML = content;
+            container.insertBefore(div, buttonNew)
+            
+        })
+    }
+    putBooksOnScreen();
